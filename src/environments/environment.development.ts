7080847c21dfb3a0fe1d6b9902ca43bd7e59b8a2
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-04 12:38:58 -0500

    creacion de json server

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   baseUrl: "http://localhost:3000",
+  baseMockApiUrl: "http://localhost:3001"
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   baseUrl: "http://localhost:3000",
+  baseMockApiUrl: "http://localhost:3001"
 };
