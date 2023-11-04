d0ad2f07cfa6cf48576132a857477893be3e96b3
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-04 02:17:57 -0500

    agregue interfaces y pipe para el observable

---
+++ src/app/auth/interfaces/user.interface.ts
@@ -0,0 +1,12 @@
+export interface User {
+  _id: string;
+  email: string;
+  name: string;
+  isActive: boolean;
+  roles: string[];
+}
+
+export interface LoginResponse {
+  user: User;
+  token: string;
+}
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { tap } from "rxjs";
+import { LoginResponse, User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,18 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
+  private _user?: User;
 
   constructor( private http: HttpClient ) { }
 
   login(email: string, password: string){
-    return this.http.post(`${this.baseUrl}/auth/login`, {email, password});
+    return this.http.post<LoginResponse>(`${this.baseUrl}/auth/login`, {email, password})
+        .pipe(
+          tap( resp => this._user = resp.user),
+          tap( resp => {
+            localStorage.setItem('token', resp.token);
+          })
+        );
   }
 
 }
