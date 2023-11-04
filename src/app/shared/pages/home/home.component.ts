f5b3b4b37d2abe2c890755fce5be2d6e9a36a201
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-04 12:55:27 -0500

    mostrar lista de productos

--- src/app/shared/pages/home/home.component.ts
+++ src/app/shared/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  public products: Product[] = [];
+
+  constructor( private productService: ProductService ) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts()
+      .subscribe( products => {
+        this.products = products;
+      });
+  }
 
 }
