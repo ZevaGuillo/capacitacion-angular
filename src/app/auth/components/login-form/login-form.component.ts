bd40e11bc383e44bc4b720490c35093b6755b4c4
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-03 20:07:44 -0500

    creacion de modulo de auth y creacion de login form

---
+++ src/app/auth/components/login-form/login-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent {
+
+// NG MODEL
+
+  // userLogin: { username: string, password: string } = {
+  //   username: 'guillermozev',
+  //   password: '123456'
+  // }
+
+  // classError: string = 'text-red-200';
+
+  // constructor() { }
+
+  // login() {
+  //   console.log(this.userLogin);
+  // }
+
+
+
+}
